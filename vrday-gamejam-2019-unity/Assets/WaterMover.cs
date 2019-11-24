c1dff2fc4a39163035d4b7fe69f711a8e8fc10a0
Unknown <ciszekrm@vcu.edu>
2019-11-23 20:11:47 -0500

    Wheelie action

--- vrday-gamejam-2019-unity/Assets/WaterMover.cs
+++ vrday-gamejam-2019-unity/Assets/WaterMover.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR.InteractionSystem;
 
 public class WaterMover : MonoBehaviour
 {
@@ -8,11 +9,15 @@ public class WaterMover : MonoBehaviour
     public float HeightOffset;
     public float TimeMultiplier;
     private float Timer =0.0f;
-    private Vector3 initialLocation; 
+    private Vector3 initialLocation;
+    public GameObject shipwheel;
+    private CircularDrive _wheeldrive;
     // Start is called before the first frame update
     void Start()
     {
         initialLocation = transform.position;
+        _wheeldrive = shipwheel.GetComponent(typeof(CircularDrive)) as CircularDrive;
+
     }
 
     // Update is called once per frame
@@ -20,5 +25,6 @@ public class WaterMover : MonoBehaviour
     {
         Timer += Time.deltaTime;
         transform.position = new Vector3(0, Mathf.Sin(Timer * TimeMultiplier) * HeightOffset, 0) + initialLocation;
+        transform.rotation = new Quaternion(0, 0, _wheeldrive.outAngle,0);
     }
 }
